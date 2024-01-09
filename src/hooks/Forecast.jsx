6f26fa496be584440ccff6fff733934a2e73f08e
Mortreddd <emmanmale@gmail.com>
2024-01-09 20:07:10 +0800

    Update

--- src/hooks/Forecast.jsx
+++ src/hooks/Forecast.jsx
@@ -1,5 +1,5 @@
-// const API_KEY = import.meta.env.VITE_API_KEY;
-// const API_URL = import.meta.env.VITE_API_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+const API_URL = import.meta.env.VITE_API_URL;
 export async function fetchForecastByLocation(location) {
   const latitude = location.coords.latitude;
   const longtitude = location.coords.longtitude;
